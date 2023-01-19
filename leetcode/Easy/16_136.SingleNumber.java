8d5ce88c072b1c8c510312152394dbaab08ceae3
Mohammed Anfal Sharief <92685449+anfal17@users.noreply.github.com>
2023-01-19 23:15:33 +0530

    Create 16_136.SingleNumber.java

---
+++ leetcode/Easy/16_136.SingleNumber.java
@@ -0,0 +1,21 @@
+/*
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+*/
+//Solution 1
+class Solution {
+    public int singleNumber(int[] nums) {
+        int number = 0;
+
+        for(int i =0 ; i < nums.length ; i++){
+            number = number ^ nums[i]; //0 ^ 2 = 2 , 2  ^ 2 = 0 , 0 ^ 1 = 1 ---> return 1 unique 
+        }
+        return number;
+    }
+}
