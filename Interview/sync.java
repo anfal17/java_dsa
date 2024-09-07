a41c799c883f80655773ba81a5736f415c1879e7
Mohammed Anfal Sharief <92685449+anfal17@users.noreply.github.com>
2024-09-07 23:22:49 +0530

    Update sync.java

--- Interview/sync.java
+++ Interview/sync.java
@@ -61,3 +61,27 @@ class HelloWorld {
 //kadanes algo , weighted strings
 
 //java core - static, final , finalise , volatile, serializeinterface
+//layered architecture why nodejs, mongodb,?? nodes clusters, kubernetes
+
+
+//maximum subarrays sum
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+
+public static void main(String[] args) {
+    // int[] arr = {-2 , -1 ,2 -10, -30,50};
+    int[] arr = {-2 , -50 ,-3, -4,0 };
+        
+    int curSum = 0;
+    int maxSum = Integer.MIN_VALUE;
+
+        
+    for(int i = 0; i < arr.length ;i++){
+        curSum += arr[i];
+        if(curSum > maxSum) {maxSum = curSum;} 
+        if(curSum < 0) {curSum = 0;}
+            
+    }    
+    System.out.println(maxSum);
+}
