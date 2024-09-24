89b7ef6f8ce2aea7589d64da7051f65eacf900a1
anfal17 <mohdanfal17@gmail.com>
2024-09-24 22:39:14 +0530

    updates

---
+++ Java_IO_File/SerializationObjects.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+//serialization is to store objects contents in java
+//static and transient not stored
+//serializable interface in java to that class which is serialized
+//first FileOutputStream then ObjectOutputStream
+
+
+//deserialization - similar
+//first FileInputStream then ObjectInputStream
+
+
+public class SerializationObjects {
+
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ MultiThreading/MainThread.java
@@ -0,0 +1,5 @@
+public class MainThread {
+    public static void main(String[] args) {
+        
+    }
+}
